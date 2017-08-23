096b1a473f84143687c99e5b85be1e33915707e4
Paulo <p.moreno@ltp.nl>
2017-08-23 16:09:55 +0200

    start methods on App

--- src/App.php
+++ src/App.php
@@ -4,11 +4,19 @@ namespace TeamChris;
 
 class App
 {
-    public function checkPlaces(array $placeIds): array
+    public function rateAPlace(string $id, $vote)
     {
+     //add a new entry to the db
     }
 
-    public function rateAPlace(string $place_id, bool $vote): array
+    public function checkPlaces(array $listOfIds): array
     {
+
+    }
+
+    private function readFromDatabase(): array
+    {
+        //TODO:MIDORI
+        //get a list of all entries in our database
     }
 }
