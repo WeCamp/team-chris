00e937526f34d10aeedd9247ee3c3a6709d7ed0f
Paulo <p.moreno@ltp.nl>
2017-08-23 16:35:44 +0200

    checkForPlaces app

--- src/App.php
+++ src/App.php
@@ -4,19 +4,46 @@ namespace TeamChris;
 
 class App
 {
-    public function rateAPlace(string $id, $vote)
+    /**
+     * @var \PDO
+     */
+    private $db;
+
+    public function __construct(\PDO $db)
     {
-     //add a new entry to the db
+        $this->db = $db;
+    }
+
+    public function rateAPlace(string $id, int $vote)
+    {
+        //add a new entry to the db
     }
 
     public function checkPlaces(array $listOfIds): array
     {
-
+        $results = [];
+        foreach ($listOfIds as $id)
+        {
+            $results[] = [
+                'id'=> $id,
+                'ratings' => [
+                    'category' => 'LGBT',
+                    'upAmount' => countVotes($id)['upAmount'],
+                    'downAmount' => countVotes($id)['downAmount']
+                ]
+          ];
+        }
+        return $results;
     }
 
-    private function readFromDatabase(): array
+
+    private function countVotes($id): array
     {
-        //TODO:MIDORI
-        //get a list of all entries in our database
+        //query
+    }
+
+    private function getEntryFromDatabase($id)
+    {
+
     }
 }
