433142474fff80f06755792a1a79a10954c0a9de
Toby Griffiths <toby@cubicmushroom.co.uk>
2017-08-25 17:52:11 +0200

    Use main.js file rather than general as root JS file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './public/js/general.js',
+  entry: './public/js/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './public/js')
